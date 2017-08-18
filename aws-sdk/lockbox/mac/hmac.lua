322ad059df1472b171d13820d69ddc5522893a91
Björn Ritzl <bjorn.ritzl@king.com>
2017-08-18 08:45:59 +0200

    Removed semi-colons

--- aws-sdk/lockbox/mac/hmac.lua
+++ aws-sdk/lockbox/mac/hmac.lua
@@ -1,85 +1,85 @@
-local Bit = require("aws-sdk.lockbox.util.bit");
-local Stream = require("aws-sdk.lockbox.util.stream");
-local Array = require("aws-sdk.lockbox.util.array");
+local Bit = require("aws-sdk.lockbox.util.bit")
+local Stream = require("aws-sdk.lockbox.util.stream")
+local Array = require("aws-sdk.lockbox.util.array")
 
-local XOR = Bit.bxor;
+local XOR = Bit.bxor
 
 local HMAC = function()
 
-	local public = {};
-	local blockSize = 64;
-	local Digest = nil;
-	local outerPadding = {};
+	local public = {}
+	local blockSize = 64
+	local Digest = nil
+	local outerPadding = {}
 	local innerPadding = {}
-	local digest;
+	local digest
 
 	public.setBlockSize = function(bytes)
-		blockSize = bytes;
-		return public;
+		blockSize = bytes
+		return public
 	end
 
 	public.setDigest = function(digestModule)
-		Digest = digestModule;
-		digest = Digest();
-		return public;
+		Digest = digestModule
+		digest = Digest()
+		return public
 	end
 
 	public.setKey = function(key)
-		local keyStream;
+		local keyStream
 
 		if(Array.size(key) > blockSize) then
 			keyStream = Stream.fromArray(Digest()
 						.update(Stream.fromArray(key))
 						.finish()
-						.asBytes());
+						.asBytes())
 		else
-			keyStream = Stream.fromArray(key);
+			keyStream = Stream.fromArray(key)
 		end
 
-		outerPadding = {};
-		innerPadding = {};
+		outerPadding = {}
+		innerPadding = {}
 
 		for i=1,blockSize do
-			local byte = keyStream();
-			if byte == nil then byte = 0x00; end
-			outerPadding[i] = XOR(0x5C,byte);
-			innerPadding[i] = XOR(0x36,byte);
+			local byte = keyStream()
+			if byte == nil then byte = 0x00 end
+			outerPadding[i] = XOR(0x5C,byte)
+			innerPadding[i] = XOR(0x36,byte)
 		end
 
-		return public;
+		return public
 	end
 
 	public.init = function()
 		digest	.init()
-				.update(Stream.fromArray(innerPadding));
-		return public;
+				.update(Stream.fromArray(innerPadding))
+		return public
 	end
 
 	public.update = function(messageStream)
-		digest.update(messageStream);
-		return public;
+		digest.update(messageStream)
+		return public
 	end
 
 	public.finish = function()
-		local inner = digest.finish().asBytes();
+		local inner = digest.finish().asBytes()
 		digest	.init()
 				.update(Stream.fromArray(outerPadding))
 				.update(Stream.fromArray(inner))
-				.finish();
+				.finish()
 
-		return public;
+		return public
 	end
 
 	public.asBytes = function()
-		return digest.asBytes();
+		return digest.asBytes()
 	end
 
 	public.asHex = function()
-		return digest.asHex();
+		return digest.asHex()
 	end
 
-	return public;
+	return public
 
 end
 
-return HMAC;
+return HMAC
--- aws-sdk/lockbox/util/stream.lua
+++ aws-sdk/lockbox/util/stream.lua
@@ -1,103 +1,103 @@
-local Queue = require("aws-sdk.lockbox.util.queue");
-local String = string;
+local Queue = require("aws-sdk.lockbox.util.queue")
+local String = string
 
-local Stream = {};
+local Stream = {}
 
 
 Stream.fromString = function(string)
-	local i=0;
+	local i=0
 	return function()
-		i=i+1;
+		i=i+1
 		if(i <= String.len(string)) then
-			return String.byte(string,i);
+			return String.byte(string,i)
 		else
-			return nil;
+			return nil
 		end
 	end
 end
 
 
 Stream.toString = function(stream)
-	local array = {};
-	local i=1;
+	local array = {}
+	local i=1
 
-	local byte = stream();
+	local byte = stream()
 	while byte ~= nil do
-		array[i] = String.char(byte);
-		i = i+1;
-		byte = stream();
+		array[i] = String.char(byte)
+		i = i+1
+		byte = stream()
 	end
 
-	return table.concat(array,"");
+	return table.concat(array,"")
 end
 
 
 Stream.fromArray = function(array)
-	local queue = Queue();
-	local i=1;
+	local queue = Queue()
+	local i=1
 
-	local byte = array[i];
+	local byte = array[i]
 	while byte ~= nil do
-		queue.push(byte);
-		i=i+1;
-		byte = array[i];
+		queue.push(byte)
+		i=i+1
+		byte = array[i]
 	end
 
-	return queue.pop;
+	return queue.pop
 end
 
 
 Stream.toArray = function(stream)
-	local array = {};
-	local i=1;
+	local array = {}
+	local i=1
 
-	local byte = stream();
+	local byte = stream()
 	while byte ~= nil do
-		array[i] = byte;
-		i = i+1;
-		byte = stream();
+		array[i] = byte
+		i = i+1
+		byte = stream()
 	end
 
-	return array;
+	return array
 end
 
 
-local fromHexTable = {};
+local fromHexTable = {}
 for i=0,255 do
-	fromHexTable[String.format("%02X",i)]=i;
-	fromHexTable[String.format("%02x",i)]=i;
+	fromHexTable[String.format("%02X",i)]=i
+	fromHexTable[String.format("%02x",i)]=i
 end
 
 Stream.fromHex = function(hex)
-	local queue = Queue();
+	local queue = Queue()
 
 	for i=1,String.len(hex)/2 do
-		local h = String.sub(hex,i*2-1,i*2);
-		queue.push(fromHexTable[h]);
+		local h = String.sub(hex,i*2-1,i*2)
+		queue.push(fromHexTable[h])
 	end
 
-	return queue.pop;
+	return queue.pop
 end
 
 
 
-local toHexTable = {};
+local toHexTable = {}
 for i=0,255 do
-	toHexTable[i]=String.format("%02X",i);
+	toHexTable[i]=String.format("%02X",i)
 end
 
 Stream.toHex = function(stream)
-	local hex = {};
-	local i = 1;
+	local hex = {}
+	local i = 1
 
-	local byte = stream();
+	local byte = stream()
 	while byte ~= nil do
-		hex[i] = toHexTable[byte];
-		i=i+1;
-		byte = stream();
+		hex[i] = toHexTable[byte]
+		i=i+1
+		byte = stream()
 	end
 
-	return table.concat(hex,"");
+	return table.concat(hex,"")
 end
 
-return Stream;
+return Stream
