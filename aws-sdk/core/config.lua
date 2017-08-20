39f7ab264f0473bca88f6655795e84b3859e8818
Björn Ritzl <bjorn.ritzl@king.com>
2017-08-20 09:18:28 +0200

    Added config for Defold, Corona and LuaSocket

--- aws-sdk/core/config.lua
+++ aws-sdk/core/config.lua
@@ -14,11 +14,49 @@ local M = {}
 -- The response table is expected to have the following fields:
 --   * status - Status code (number)
 --   * response - The response data (string)
---   * header - Response headers (table, key-value pairs)
+--   * headers - Response headers (table, key-value pairs)
 function M.http_request(uri, method, headers, post_data, callback)
 	print("No-op http_request. Replace with your own implementation")
 	print(uri, method, headers, post_data, callback)
 end
 
+
+--- Use the Defold http.request() function to make HTTP requests
+function M.use_defold()
+	M.http_request = function(uri, method, headers, post_data, callback)
+		http.request(uri, method, function(_, _, response)
+			callback(response)
+		end, headers, post_data)
+	end
+end
+
+
+--- Use the LuaSocket http module to make HTTP requests
+function M.use_luasocket(http, ltn12)
+	M.http_request = function(uri, method, headers, post_data, callback)
+		local response = {}
+		local result, code, response_headers, status = http.request({
+			method = method,
+			url = uri,
+			headers = headers,
+			source = ltn12.source.string(post_data),
+			sink = ltn12.sink.table(response)
+		})
+		callback({ status = code, response = response and response[1] or nil, headers = response_headers })
+	end
+end
+
+
+--- Use the Corona network.request function to make HTTP requests
+function M.use_corona()
+	M.http_request = function(uri, method, headers, post_data, callback)
+		network.request(uri, method, function(event)
+			callback({ status = event.status, headers = event.responseHeaders, response = event.response })
+		end, {
+			headers = headers,
+			body = post_data,
+		})
+	end
+end
 
 return M
